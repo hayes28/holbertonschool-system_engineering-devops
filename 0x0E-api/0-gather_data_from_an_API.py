e6c0fce34f231895d66cc3ef2442a539e667f8ea
Sourcery AI <>
2023-10-21 20:17:45 +0000

    'Refactored by Sourcery'

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -4,42 +4,40 @@ Python script that, using this REST API,
 for a given employee ID, returns information
 about his/her TODO list progress.
 """
+
 import requests
 from sys import argv
 
 
 if __name__ == '__main__':
     if len(argv) != 2:
-        print("Usage: {} EMPLOYEE_ID".format(argv[0]))
+        print(f"Usage: {argv[0]} EMPLOYEE_ID")
         exit(1)
 
     EMPLOYEE_ID = argv[1]
-    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
-        EMPLOYEE_ID)
+    url = f"https://jsonplaceholder.typicode.com/users/{EMPLOYEE_ID}/todos"
     response = requests.get(url)
 
     if response.status_code != 200:
-        print("Error: Request failed with status code {}".format(
-            response.status_code))
+        print(f"Error: Request failed with status code {response.status_code}")
         exit(1)
 
     todos = response.json()
     total_tasks = len(todos)
     done_tasks = [todo for todo in todos if todo['completed']]
 
-    employee_name_url = "https://jsonplaceholder.typicode.com/users/{}".format(
-        EMPLOYEE_ID)
+    employee_name_url = f"https://jsonplaceholder.typicode.com/users/{EMPLOYEE_ID}"
     response_name = requests.get(employee_name_url)
 
     if response_name.status_code != 200:
-        print("Error: Request failed with status code {}".format(
-            response_name.status_code))
+        print(f"Error: Request failed with status code {response_name.status_code}")
         exit(1)
 
     employee_name = response_name.json().get('name')
 
-    print("Employee {} is done with tasks({}/{}):".format(
-        employee_name, len(done_tasks), total_tasks))
+    print(
+        f"Employee {employee_name} is done with tasks({len(done_tasks)}/{total_tasks}):"
+    )
 
     for todo in done_tasks:
         print("\t {} {}".format('\t', todo['title']))
